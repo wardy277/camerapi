eb62af4bcce9696c709d17e67ca236eeccfd9c6d
Chris Ward <wardy277@gmail.com>
2015-01-28 17:17:21 +0000

    adding moment js and using that for the sync time method

--- ajax/update_time.php
+++ ajax/update_time.php
@@ -1,10 +1,17 @@
 <?php
 
-$time = $_POST['time'];
+$date_time = $_POST['date_time'];
 
 //validate date 
-$valid = preg_match("/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/", $time);
+$valid = preg_match("/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/", $date_time);
 
-if(($valid)){
-	file_put_contents('/tmp/update_time', $time);
+$output['success'] = false;
+
+if($valid){
+	file_put_contents('/tmp/update_time', $date_time);
+	$output['success'] = true;
 }
+
+echo json_encode( $output );
+
+?>
--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,11 @@
 
-
 function sync_time(){
-	var now = new Date();
-	var strDateTime = [[AddZero(now.getDate()), AddZero(now.getMonth() + 1), now.getFullYear()].join("/"), [AddZero(now.getHours()), AddZero(now.getMinutes())].join(":"), now.getHours() >= 12 ? "PM" : "AM"].join(" ");
+	var date_time = moment().format('YYYY-MM-DD HH:mm:ss')
 
-	$.post('/ajax/update_time.php, {time:strDateTime});
+	$.post('/ajax/update_time.php', {date_time:date_time});
 }
 
-//Pad given value to the left with "0"
-function AddZero(num) {
-    return (num >= 0 && num < 10) ? "0" + num : num + "";
+
+function init(){
+
 }
